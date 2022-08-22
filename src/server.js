2e723065d6944d9df6c32769857bf73b74ce8a5e
philomeno <106666729+philomeno@users.noreply.github.com>
2022-08-22 16:51:14 +0100

    port corrected

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ require("./db/connection"); //first thing is to run db connection
 const express = require("express");
 const userRouter = require("./user/routes");
 const app = express();
+const serverPort = 5001;
+const port = process.env.PORT || serverPort;
+
 
 //add relevant routes and controllers to app before listen runs
 app.use(express.json()); //This tells the server it will always send and receive JSON
 app.use(userRouter);
 
-app.listen(5000, ()=> {
-    console.log("listening on port 5000")
+app.listen(port, ()=> {
+    console.log("listening on port ", port)
 })
